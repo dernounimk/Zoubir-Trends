8b946a20d592be1f3c6dc0d42210dc5fd4c10374
dernounimk <mohameddernouni13@gmail.com>
2025-10-11 17:32:13 +0100

    Fix API baseURL for production

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,11 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-	baseURL: import.meta.mode === "development" ? "http://192.168.143.229:5000/api" : "/api",
-	withCredentials: true, // send cookies to the server
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://192.168.143.229:5000/api"
+      : "https://zoubir-trends-backend.onrender.com/api",
+  withCredentials: true,
 });
 
 export default axiosInstance;
